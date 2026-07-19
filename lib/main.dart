f3c081a733ef6279cb5bbbab60baa690bbb16a5f
afiabasher3-svg <afiabasher3@gmail.com>
2026-07-20 01:18:48 +0600

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'screens/login_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/reset_password_screen.dart';
 
 const supabaseUrl = 'https://ncxpgtajpyhtdqmqzeud.supabase.co';
 const supabaseKey = 'sb_publishable_8H9yZx6lXRhifYD8NwFmzQ_hTzEDfhI';
+
+final navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,18 +15,38 @@ void main() async {
   runApp(const WordNinjaApp());
 }
 
-class WordNinjaApp extends StatelessWidget {
+class WordNinjaApp extends StatefulWidget {
   const WordNinjaApp({super.key});
 
   @override
+  State<WordNinjaApp> createState() => _WordNinjaAppState();
+}
+
+class _WordNinjaAppState extends State<WordNinjaApp> {
+  @override
+  void initState() {
+    super.initState();
+    Supabase.instance.client.auth.onAuthStateChange.listen((data) {
+      final event = data.event;
+      if (event == AuthChangeEvent.passwordRecovery) {
+        navigatorKey.currentState?.pushReplacement(
+          MaterialPageRoute(builder: (_) => const ResetPasswordScreen()),
+        );
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final session = Supabase.instance.client.auth.currentSession;
     return MaterialApp(
       title: 'Word Ninja',
+      navigatorKey: navigatorKey,
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: const Color(0xFF0A0A1A),
       ),
-      home: const LoginScreen(),
+      home: session != null ? const HomeScreen() : const LoginScreen(),
     );
   }
 }
