5757a0056cb2e090703bf8bd0af57268ffb07b8f
afiabasher3-svg <afiabasher3@gmail.com>
2026-08-08 19:51:04 +0600

    topic.dart

--- lib/models/topic.dart
+++ lib/models/topic.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Topic {
+  final int id;
+  final String name;
+  final String emoji;
+  final Color bgColor;
+  final Color themeColor;
+
+  Topic({
+    required this.id,
+    required this.name,
+    required this.emoji,
+    required this.bgColor,
+    required this.themeColor,
+  });
+
+  static Color hexToColor(String hex) => Color(int.parse('FF$hex', radix: 16));
+
+  factory Topic.fromMap(Map<String, dynamic> map) {
+    return Topic(
+      id: map['id'] as int,
+      name: map['name'] as String,
+      emoji: map['emoji'] as String,
+      bgColor: hexToColor(map['bg_color'] as String),
+      themeColor: hexToColor(map['theme_color'] as String),
+    );
+  }
+}
 
